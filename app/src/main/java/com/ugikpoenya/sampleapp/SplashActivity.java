fbb7a775f9c054135216b36af3cfe3bc2cd978f3
Sugeng Riyanto PC <ugikpoenya@gmail.com>
2024-07-28 15:26:31 +0700

    Update SDK Server

--- app/src/main/java/com/ugikpoenya/sampleapp/SplashActivity.java
+++ app/src/main/java/com/ugikpoenya/sampleapp/SplashActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ugikpoenya.appmanager.FirebaseManager;
 import com.ugikpoenya.appmanager.Prefs;
 import com.ugikpoenya.appmanager.ServerManager;
 
@@ -19,11 +20,15 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        FirebaseManager firebaseManager = new FirebaseManager();
+        firebaseManager.setBaseUrl(this,"https://ugikpoenya-app-manager-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        firebaseManager.setApiKey(this,"-O2sR7wdll6d9-nIrCOZ");
+
         ServerManager serverManager = new ServerManager();
         serverManager.setBaseUrl(this, "https://master.ugikpoenya.net/api/");
         serverManager.setApiKey(this, "DA8BB129F7C1ED5BD07046961C995A77");
 
-        serverManager.getItemDelay(this, 3000, () -> {
+        firebaseManager.getItemDelay(this, 3000, () -> {
             if (new Prefs(this).getVersion_code() == BuildConfig.VERSION_CODE) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             } else {
